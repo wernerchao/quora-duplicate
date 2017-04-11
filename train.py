10ee75cff184196a30bc4b35418467673be6adb2
Glen Chao <glenchao@outlook.com>
2017-04-11 08:57:22 -0700

    Simple read file

--- train.py
+++ train.py
@@ -1,1 +1,10 @@
 # training
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+pal = sns.color_palette()
+
+train = pd.read_csv('./data/train.csv')
+test = pd.read_csv('./data/test.csv')
