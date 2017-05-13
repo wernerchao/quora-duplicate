68e7a39c82c798036e6d4bade57a46131f5a4450
Werner Chao <oahcrenrew@gmail.com>
2017-05-12 23:40:17 -0400

    added spark pipeline

---
+++ spark_pipeline.py
@@ -0,0 +1,26 @@
+from pyspark.ml.feature import HashingTF, IDF, Tokenizer, StopWordsRemover
+from pyspark.ml.classification import LogisticRegression
+from pyspark.ml import pipeline
+
+print 'Start data preprocessing...'
+df = sqlContext.read.load('./train.csv', 
+                          format='com.databricks.spark.csv', 
+                          header='true', 
+                          inferSchema='true')
+
+df = df.cache()
+
+tokenizer = Tokenizer(inputCols=['question1', 'question2'], outputCol='words')
+remover = StopWordsRemover(inputCol="words", outputCol="filtered")
+hashingTF = HashingTF(inputCol="filtered", outputCol="rawFeatures", numFeatures=1000)
+idf = IDF(inputCol="rawFeatures", outputCol="features")
+
+# pipeline = Pipeline(stages=[tokenizer, remover, hashingTF, idf])
+# df = pipeline.fit(df)
+df = tokenizer.transform(df)
+df = remover.transform(df)
+df = hashingTF.transform(df)
+df = idf.fit(df).transform(df)
+df.select("features").show()
+
+lr = LogisticRegression(maxIter=10, regParam=0.001)
